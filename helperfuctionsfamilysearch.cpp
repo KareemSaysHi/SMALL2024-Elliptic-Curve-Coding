d47d0d99412181714adb880b83c7ae53cdd517d4
m-ht3 <90869833+m-ht3@users.noreply.github.com>
2024-08-02 11:28:43 -0400

    Add files via upload
    Updated helper functions and quartic grapher for conducting a family search

---
+++ helperfuctionsfamilysearch.cpp
@@ -0,0 +1,307 @@
+#include "helperfunctions.h"
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+// Function to calculate a^b mod p without overflow
+long power_mod_p(long a, int b, int p) {
+    long result = 1;
+    while (b > 0) {
+        if (b % 2 == 1) {
+            result = (result * a) % p;
+        }
+
+        b = b >> 1;
+        a = (a * a) % p;
+    }
+    return ((result % p) + p ) % p;
+}
+
+// Recursive function to generate all sequences of a given length
+void generateSequences(const std::vector<int>& nums, int length, std::vector<int>& current, std::vector<std::vector<int>>& result) {
+    if (length == 0) {
+        result.push_back(current);
+        return;
+    }
+    
+    for (int num : nums) {
+        current.push_back(num);
+        generateSequences(nums, length - 1, current, result);
+        current.pop_back(); // backtrack
+    }
+}
+
+// Function to generate all possible sequences of a given length
+std::vector<std::vector<int>> generateAllSequences(const std::vector<int>& nums, int length) {
+    std::vector<std::vector<int>> result;
+    std::vector<int> current;
+    generateSequences(nums, length, current, result);
+    return result;
+}
+
+// Function to calculate Legendre symbol
+int legendre_symbol(int x, int p) {
+    if (x % p == 0) {
+        return 0;
+    } else if (power_mod_p(x, int((p-1)/2), p) == 1) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+//find inverse of a mod p
+int inverse(int a, int p) {
+    return power_mod_p(a, p-2, p);
+}
+
+// Returs true if fourth power mod p, otherwise returns false
+bool isFourthPower(int x, int p) {
+    if (p % 4 == 1) {
+        if (power_mod_p(x, int((p-1)/4), p) == 1) { //1 iff x is a fourth power mod p since multiplicative group is cyclic
+            return true;
+        }
+    } else {
+        if (power_mod_p(x, int((p-1)/2), p) == 1) { //in 3 mod 4, if you're a square, you're a fourth power!
+            return true;
+        }
+    }
+    return false;
+}
+
+vector<int> findQuarticResidueClasses(int p) {
+    //need to make this work for p = 2, 3, maybe 5
+    if (p == 2) {
+        return vector<int>{0, 1};
+    } else if (p == 3) {
+        return vector<int>{0, 1, 2};
+    } else if (p == 5) {
+        return vector<int>{0, 1, 2, 3, 4}; //all in different
+    }
+    int thingToCheck = 0;
+    bool inPrevResClass = false;
+    vector<int> classReps;
+    classReps.push_back(0);
+    for (int i = 1; i < p; i++) {
+        inPrevResClass = false;
+        for (int rep : classReps) {
+            thingToCheck = i * inverse(rep, p) % p;
+            if (isFourthPower(thingToCheck, p)) {
+                inPrevResClass = true;
+                break;
+            }
+        }
+
+        if (!inPrevResClass) {
+            classReps.push_back(i);
+        }
+            
+        if (p % 4 == 3) {
+            if (classReps.size() >= 3) { //we have 2 quartic classes in 3 mod 4
+                return classReps;
+            }
+        } else if (p % 4 == 1) {
+            if (classReps.size() >= 5) { //we have 4 quartic class in 1 mod 4
+                return classReps;
+            }
+        }
+        
+    }
+
+    cout << "something very bad has happened if this is printed" << endl;
+    return classReps;
+}
+
+// Function to generate prime numbers up to k
+vector<int> generate_primes_in_range(int lower, int upper) {
+    vector<int> primes;
+    for (int num = 2; num < upper; num++) {
+        bool is_prime = true;
+        for (int i = 2; i <= sqrt(num); i++) {
+            if (num % i == 0) {
+                is_prime = false;
+                break;
+            }
+        }
+        if (is_prime && (num >= lower)) {
+            primes.push_back(num);
+        }
+    }
+    return primes;
+}
+
+static vector<int> cipollamult(vector<int>& term1, vector<int>& term2, int fieldextension, int p) {
+    // Ensure the input vectors are of size 2
+    if (term1.size() != 2 || term2.size() != 2) {
+        throw std::invalid_argument("Both input vectors must be of size 2.");
+    }
+    
+    int a = term1[0];
+    int b = term1[1];
+    int c = term2[0];
+    int d = term2[1];
+
+    vector<int> result = {( a * c % p + (((fieldextension * b) % p) * d) % p) % p, (b * c % p + a * d % p) % p };
+    
+    return result;
+}
+
+//b is the power here
+static vector<int> cipolla_power_mod_p(vector<int>& term, int b, int fieldextension, int p) {
+    vector<int> result = {1, 0};
+    while (b > 0) {
+        if (b % 2 == 1) {
+            //result = (result * a) % p;
+            result = cipollamult(result, term, fieldextension, p);
+        }
+
+        b = b >> 1;
+
+        //a = (a * a) % p;
+        term = cipollamult(term, term, fieldextension, p);
+    }
+    return result;
+}
+
+int squareroot(int n, int p) {
+    n = ((n % p)+p) % p;
+    if (power_mod_p(n, int((p-1)/2), p) != 1) {
+        cout << "No square root exists" << endl;
+        cout << "n = " << n << ", p = " << p << endl;
+        return -15000000;
+    }
+    
+    if (p % 4 == 3) {
+        int sqrt = power_mod_p(n, int((p+1)/4), p); //neat trick: this is a solution to a^2 = b
+        return sqrt;
+    } else { //we need to apply cipolla's algo for x^2 = n mod p
+        //step 1: find "a" such that a^2-n is not a square mod p
+        int a = 0;
+        int thingToEvaluate = 0;
+        for (int i = 1; i < p; i++) {
+            thingToEvaluate = (((power_mod_p(i, 2, p) - n) % p) + p) % p;
+            if (power_mod_p(thingToEvaluate, int((p-1)/2), p) == p-1) { //NOT -1
+                a = i;
+                break;
+            }
+        }
+        //step 2: compute thing in cipolla
+        //we are adjoining a square root of a^2 - n, so we need to work in the field Z_p[sqrt(a^2-n)]
+        int asquaredminusn = (((a*a - n) % p) + p) % p;
+        vector<int> guyToRaisePowerTo = {a, 1};
+        vector<int> result = cipolla_power_mod_p(guyToRaisePowerTo, (p+1)/2, asquaredminusn, p);
+        
+        return (result[0] % p + p) % p;
+    }
+}
+
+// get A and B values from CSV file
+int getAp(int p, int A, int B) {
+    int c = 0;
+    int repline = 0;
+
+    vector<int> reps = findQuarticResidueClasses(p);
+
+    string filename = "classdata/file_" + to_string(p) + ".csv";
+
+    ifstream file;
+    file.open(filename);
+    //step 1: figure out what residue class A is in
+    for (int i = 0; i <= reps.size(); i++) {
+        if (reps[i] == 0) {
+            if (A == 0) {
+                c = 0;
+                repline = i;
+                break;
+            }
+        }
+
+        if (isFourthPower((A * inverse(reps[i], p)) % p, p)) {
+            //cout << "found a fourth power - " << reps[i] << endl;
+            //cout << (A * inverse(reps[i], p)) % p << endl;
+            //cout << isFourthPower((A * inverse(reps[i], p)) % p, p) << endl;
+            c = reps[i];
+            repline = i;
+            break;
+        }
+    }
+
+    if (c == 0) {
+        int readThing = get_value_from_file(p, repline, B, file);
+
+        file.close();
+        return readThing;
+    }
+
+    int lfourth = A * inverse(c, p) % p;
+
+    //step 2: calulcate lsixth
+    int lsquare = squareroot(lfourth, p);
+    //int lsquare = 1;
+    int lsixth = lsquare * lfourth % p;
+
+    //step 3: calculate B and lookup
+    B = B * inverse(lsixth, p) % p;
+
+    int readThing = get_value_from_file(p, repline, B, file);
+
+    file.close();
+    return readThing;
+
+}
+
+// Function to get value from a CSV file
+int get_value_from_file(int prime, int repline, int b, ifstream &file) {
+
+    string line;
+    int target_line = prime * repline + b + 1; //+1 to account for header
+    int current_line = 0;
+    
+    file.clear();           // Clear any error flags
+    file.seekg(0, std::ios::beg); // Move the file pointer to the beginning
+
+    while (getline(file, line)) {
+        if (current_line == target_line) {
+            stringstream ss(line);
+            string item;
+            int value;
+            for (int i = 0; getline(ss, item, ','); ++i) {
+                /*if (i == 0) {
+                    cout << "rep = " << item << ", ";
+                }
+
+                if (i == 1) {
+                    cout << "b = " << item << ", ";
+                }*/
+
+                if (i == 2) {
+                    //cout << "value = " << item << "!";
+                    value = stoi(item);
+                    return value;
+                }
+            }
+        }
+        ++current_line;
+    }
+    return -1;
+}
+
+long largenummodp(vector<long>& digits, int prime) {
+    long res = 0;
+    int len = digits.size();
+    for (int i=0; i < len; i++) {
+        int temp = digits[i] % prime;
+        int pow10 = power_mod_p(10, 4*i, prime);
+        temp = (temp * pow10) % prime;
+        res = (res + temp) % prime;
+    }
+    return res;
+}
---
+++ quarticgrapherfamilysearch.cpp
@@ -0,0 +1,252 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <sstream>
+#include "helperfunctions.h"
+
+using namespace std;
+
+vector<vector<int>> getPowersOfTmodP(int p, int highestPower) {
+    vector<vector<int>> powers(p, vector<int>(highestPower, 0));
+    for (int t = 0; t < p; t++) {
+        for (int power = 0; power < highestPower; power++) {
+            powers[t][power] = power_mod_p(t, power, p);
+        }
+    }
+    return powers;
+}
+
+int calculatePoly(int t, int p, const vector<vector<int>>& powersOfT, const vector<int>& coeffs) {
+    int result = 0;
+    int count = 0;
+    for (int coeff: coeffs) {
+        result += coeff * powersOfT[t][count];
+        result = result % p;
+        count++;
+    }
+    return result;
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 5) {
+        cerr << "Usage: " << argv[0] << " <file_name> <upper_limit> <filter_on> <a> <mod b>" << endl;
+        return 1;
+    }
+
+    double thresh = 0.25; // threshold for what to write to output file if positive
+
+    int n = atoi(argv[1]); //how high do you want to go with your primes
+    int filterOn = atoi(argv[2]); //turns filter on or not
+    int filterA = atoi(argv[3]); //A
+    int filterB = atoi(argv[4]); //modB
+
+    std::vector<int> primes = generate_primes_in_range(2, n);
+
+    std::vector<int> nums = {0, 1};
+    int length = 6; // Length of sequences to generate so desired degree - 1
+    
+    // Generate sequences of coefficients
+    std::vector<std::vector<int>> coeffs = generateAllSequences(nums, length);
+    // Code for testing particular sequence to do so change
+    // Outer for loop to go over coeffs1 and inner for loop to go over coeffs2 
+    /*std::vector<int> Alist;
+    Alist.push_back(1);
+    Alist.push_back(0);
+    Alist.push_back(0);
+    Alist.push_back(0);
+    std::vector<std::vector<int>> coeffs1;
+    coeffs1.push_back(Alist);
+    
+    std::vector<int> Blist;
+    Blist.push_back(0);
+    Blist.push_back(0);
+    Blist.push_back(0);
+    Blist.push_back(1);
+    std::vector<std::vector<int>> coeffs2;
+    coeffs2.push_back(Blist);
+    */
+
+    for (const auto& seqA : coeffs) {
+        // For loop to choose A polynomial
+        // Exclude all zero sequence corresponding to A = 0
+        bool flag = true;
+        for (int i = 0; i < length; i++){
+            if (seqA[i] != 0) {
+                flag = false;
+                cout << "Entered" << endl;
+                break;
+            }
+        }
+        if (flag){
+            continue;
+        }
+
+        for (const auto& seqB : coeffs) {
+             // Exclude all zero sequence corresponding to B = 0
+            bool flag = true;
+            for (int i = 0; i < length; i++){
+                if (seqB[i] != 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                continue;
+            }
+
+            // Reinitialize for each family
+            vector<int> x; /*Primes*/
+            vector<double> y; /*Running Average*/
+            vector<double> z; /*Weighted Running Average*/
+            vector<double> w; /*Normalized Second moment*/
+
+            float runningsum = 0;
+            float weightedrunningsum = 0;
+            int primecount = 0;
+            float weightedprimecount = 0;
+            int positivecount = 0;
+            int weightedpositivecount = 0;
+
+            // For loop to choose B polynomial
+
+            for (int p : primes) {
+                if (filterOn) {
+                    if (p % filterB != filterA) {
+                        continue;
+                    }
+                }
+
+                int c = 0;
+                int value = 0;
+                int readThing = 0;
+                int repline = 0;
+
+                string filename = "classdata/file_" + to_string(p) + ".csv";
+
+                int MAX_ROWS = 5*p;
+                ifstream file(filename);
+                if(!file.is_open()){
+                    continue;
+                }
+            
+                int data[MAX_ROWS];
+                string line;
+                int row = 0;
+                while(getline(file,line) && row < MAX_ROWS+1){
+                    if (row == 0){
+                        row++;
+                    } else {
+                    stringstream ss(line);
+                    string cell;
+                    for (int i = 0; getline(ss, cell, ','); ++i) {
+                        if (i == 2) {
+                            data[row-1] = stoi(cell);
+                        }
+                    }
+                    row++;
+                    }
+                }
+                
+                file.close();
+
+                vector<int> reps = findQuarticResidueClasses(p);
+
+                for (long t = 0; t < p; ++t) {
+                    // Generate polynomials
+                    long A = 0;
+                    long B = 0;
+                    long powT = 0;
+                    for (int i = 0; i < length; i++) {
+                        if (i == 0) {
+                            powT = 1;
+                        } else {
+                            powT = power_mod_p(t, i, p);
+                        }
+                        A = (A + (seqA[i] * powT) % p) % p;
+                        B = (B + (seqB[i] * powT) % p) % p;
+                    }
+                    A = ((A % p) + p) % p;
+                    B = ((B % p) + p) % p;
+
+                    //step 1: figure out what residue class A is in
+                    for (int i = 0; i <= reps.size(); i++) {
+                        if (reps[i] == 0) {
+                            if (A == 0) {
+                                c = 0;
+                                repline = i;
+                                break;
+                            }
+                        }
+
+                        if (isFourthPower((A * inverse(reps[i], p)) % p, p)) {
+                            c = reps[i];
+                            repline = i;
+                            break;
+                        }
+                    }
+
+                    if (c == 0) {
+                        value += pow(data[B], 2);
+                    } else {
+
+                        int lfourth = A * (inverse(c, p) + p) % p;
+
+                        //step 2: calulcate lsixth
+                        int lsquare = squareroot(lfourth, p);
+                        //int lsquare = 1;
+                        int lsixth = (lsquare * lfourth) % p;
+
+                        //step 3: calculate B and lookup
+                        B = (B * inverse(lsixth, p)) % p;
+
+                        readThing = data[p * repline + B];
+                        value += pow(readThing, 2);
+                    }
+                }
+                x.push_back(p);
+                float normalizedSecondMoment = (value - pow(p, 2)) / pow(p, 1.5); //subtract by p^2 and divide by p^(1.5) to normalize
+                runningsum += normalizedSecondMoment;
+                weightedrunningsum += normalizedSecondMoment * log(p);
+                primecount += 1;
+                weightedprimecount += log(p);
+
+                // Calculate running average and weighted running average
+                if (runningsum >= 0) {
+                    positivecount ++;
+                }
+                if (weightedrunningsum >= 0) {
+                    weightedpositivecount ++;
+                }
+                w.push_back(normalizedSecondMoment);
+                y.push_back(runningsum/primecount); 
+                z.push_back(weightedrunningsum/weightedprimecount);
+            }
+            
+            // Only write to output file if positive >= thresh
+            if ((positivecount * 1.0)/primecount >= thresh) {
+                std::string textA = "dataA";
+                std::string textB = "B";
+                for (int i = 0; i < length; i++) {
+                    textA = textA + std::to_string(seqA[i]);
+                    textB = textB + std::to_string(seqB[i]);
+                }
+                std::string outputfilename = textA + textB + ".txt";
+                cout << "Finished with family " << outputfilename << endl;
+                cout << "Percent of time positive " << (positivecount * 1.0)/primecount << endl;
+
+                std::ofstream output_file(outputfilename);
+                if (!output_file.is_open()) {
+                    std::cerr << "Unable to open output file" << std::endl;
+                    return 1;
+                }
+
+                for (size_t i = 0; i < x.size(); ++i) {
+                    output_file << x[i] << "," << y[i] << "," << z[i] << "," << w[i] << std::endl;
+                }
+            }
+        }
+    }
+    return 0;
+}
