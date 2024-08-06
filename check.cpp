1b9d5c51491c64e7f715e38698f071339104fda8
m-ht3 <90869833+m-ht3@users.noreply.github.com>
2024-08-06 14:31:22 -0400

    Update check.cpp

--- check.cpp
+++ check.cpp
@@ -9,13 +9,12 @@
 
 using namespace std;
 
+// Input the range in which to check if the files corresponding to the primes are in the data files
 int main(int argc, char *argv[]) {
-    int n = atoi(argv[1]); //how high do you want to go with your primes
-    int filterOn = atoi(argv[2]); //turns filter on or not
-    int filterA = atoi(argv[3]); //A
-    int filterB = atoi(argv[4]); //modB
+    int lower = atoi(argv[1]); //where you want to start with your primes
+    int upper = atoi(argv[2]); //how high do you want to go with your primes
 
-    std::vector<long> primes = generate_primes_in_range(2, n);
+    std::vector<long> primes = generate_primes_in_range(lower, upper);
 
     vector<long> x; /*primes*/
     vector<float> y; /*Normalized Second Moment*/
@@ -25,23 +24,6 @@ int main(int argc, char *argv[]) {
 
     #pragma omp parallel for
     for (long p : primes) {
-        //vector<vector<long>> allPowersOfT = getPowersOfTmodP(p, highestPower + 1);
-
-        if (filterOn) {
-            if (p % filterB != filterA || p <= 5) {
-                continue;
-            }
-        }
-
-        long A = 0;
-        long B = 0;
-        int c = 0;
-        long value = 0;
-        int readThing = 0;
-        int repline = 0;
-
-        cout << "on prime " << p << std::endl;
-
         string filename = "classdata/file_" + to_string(p) + ".csv";
 
         int MAX_ROWS;
@@ -53,43 +35,7 @@ int main(int argc, char *argv[]) {
             cerr << "Error opening file for prime " <<  p << endl;
             continue;
         }
-
-        // int data[MAX_ROWS];
-        // int dataFor0Bs[6];
-        // int dataFor0Aps[6];
-        // std::string line;
-
-        // int lineCount = 0;
-        // while (getline(file, line)) {
-        //     std::stringstream ss(line);
-        //     string a, b;
-
-        //     if (lineCount < 6 && p % 3 == 1) {
-        //         if (getline(ss, a, ',') && getline(ss, b)) {
-        //             dataFor0Bs[lineCount] = stoi(a);
-        //             dataFor0Aps[lineCount] = stoi(b);
-        //         }
-        //     } else //(lineCount < 6 + MAX_ROWS) 
-        //     {
-        //         if (getline(ss, a)) {
-        //             if (p % 3 == 1) {
-        //                 data[lineCount-6] = stoi(a);
-        //             } else {
-        //                 data[lineCount] = stoi(a);
-        //             }
-        //         }
-        //     } 
-
-        //     ++lineCount;
-        // }
-        // file.close();
-
-        // // Check if file had correct number of lines
-        // if (abs(lineCount - MAX_ROWS) > 7) {
-        //     cout << "File for prime " << p << " has " << lineCount << " lines. Expected approximately " << MAX_ROWS << ".\n";
-        //     continue;
-        // }
+        
     }
-
     return 0;
 }
