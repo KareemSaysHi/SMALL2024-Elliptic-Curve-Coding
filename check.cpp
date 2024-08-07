211c7dc8cd17ad5e1c8b3cbedd8377c6e2cae616
m-ht3 <90869833+m-ht3@users.noreply.github.com>
2024-08-06 22:27:10 -0400

    Update check.cpp

--- check.cpp
+++ check.cpp
@@ -25,17 +25,6 @@ int main(int argc, char *argv[]) {
     #pragma omp parallel for
     for (long p : primes) {
         string filename = "classdata/file_" + to_string(p) + ".csv";
-
-        long MAX_ROWS;
-        if (p % 4 == 1) MAX_ROWS = 4*p;
-        else MAX_ROWS = 2*p;
-        ifstream file(filename);
-        
-        if (!file.is_open()){
-            cerr << "Error opening file for prime " <<  p << endl;
-            continue;
-        }
-
         long A = 0;
         long B = 0;
         int c = 0;
